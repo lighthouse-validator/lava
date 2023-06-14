3370bfb8b7d4d26ae5d66b70bdbb6a376391f2ce
Lighthouse <lighthouse.validator@gmail.com>
2023-06-14 13:44:23 +0500

    Update testnet-instructions.sh

--- validator/testnet/testnet-instructions.sh
+++ validator/testnet/testnet-instructions.sh
@@ -1,1 +1,8 @@
 # Join testnet - Manual setup with Cosmovisor
+
+## Prerequisites
+1. Verify hardware requirements are met
+2. Install package dependencies
+
+    Note: You may need to run as sudo
+    Required packages installation
